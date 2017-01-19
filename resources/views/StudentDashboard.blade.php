d89cc7cf9d58115363e74ade3bfb24783b19589b
Nuwan Perera <Nuwan Perera>
2017-01-19 23:13:51 +0530

    Init

--- resources/views/StudentDashboard.blade.php
+++ resources/views/StudentDashboard.blade.php
@@ -46,35 +46,24 @@
     <!-- Boxed -->
     <div class="boxed">
         <div class="site-header header-v2">
-            <div class="flat-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="flat-wrapper">
-                            <div class="custom-info">
-                                <span>Have any questions?</span>
-                                <span><i class="fa fa-reply"></i>themesflat@gmail.com</span> 
-                                <span><i class="fa fa-map-marker"></i>66 Nicholson St Buffalo New York US</span> 
-                                <span><i class="fa fa-phone"></i>+ 012 222 989899</span>
-                            </div>
+	  		 <div class="flat-top">
+					<div class="container">
+						<div class="row">
+							<div class="flat-wrapper">
+								<div class="custom-info">
+									 <form class="login-form">                       
+										<span><i class="fa fa-user"></i><input type="text" class="user-name" placeholder="Username"></span> 
+										<span> <input type="submit" class="login-submit" value="Log Out"></span>
+									 </form>
+								</div>
 
-                            <div class="social-links">
-                                <a href="#">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a href="#">
-                                    <i class="fa fa-facebook-official"></i>
-                                </a>
-                                <a href="#">
-                                    <i class="fa fa-google-plus"></i>
-                                </a>
-                                <a href="#">
-                                    <i class="fa fa-pinterest"></i>
-                                </a>
-                            </div>
-                        </div><!-- /.flat-wrapper -->
-                    </div><!-- /.row -->
-                </div><!-- /.container -->
-            </div><!-- /.flat-top -->
+								<div class="welcome-text">
+									<span>Welcome to the Intern Portal </span>
+								</div>
+							</div><!-- /.flat-wrapper -->
+						</div><!-- /.row -->
+					</div><!-- /.container -->
+				</div><!-- /.flat-top -->
           
             <header id="header" class="header"> 
                 <div class="header-wrap">
