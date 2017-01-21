c57c59bc3580f41efd34e777ee0a7b1fa314ad92
Nuwan Perera <Nuwan Perera>
2017-01-21 06:05:57 +0530

    Intern

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
                 <div class="header-wrap">
                     <div id="logo" class="logo">
                         <a href="index.html">
-                            <img src="{{URL::secureAsset('intern/images/logo.png')}}" alt="images">
+                            <img src="{{URL::secureAsset('intern/images/logo.png')}}" alt="images" width="60" height="60">
                         </a>
                     </div><!-- /.logo -->
                     <div class="btn-menu">
